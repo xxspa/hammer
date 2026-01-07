4531b31d44359f76850ea1eb4307a0f270cc5812
x <zheng2580369@gmail.com>
2026-01-07 15:31:25 +0800

    initialize Spring Boot application with Gradle setup and basic configurations

---
+++ src/main/java/com/newzhxu/dowload/DowloadApplication.java
@@ -0,0 +1,13 @@
+package com.newzhxu.dowload;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DowloadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DowloadApplication.class, args);
+    }
+
+}
